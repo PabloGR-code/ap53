4514286494dae4075a98a00ad823cf3042b92ba6
Pablo <pagira@alumnatflorida.es>
2026-01-30 10:55:49 +0100

    sh

--- 5.sh
+++ 5.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+    
+    while read linea;do
+
+        material=`echo $linea | awk '{print $1}'`
+        cantidad=`echo $linea | awk '{print $2}'`
+        precio=`echo $linea | awk '{print $3}'`
+
+        if [ $cantidad -lt 10 ];then
+            echo "Quedan menos de 10 $material"
+        fi
+
+        total_inventario=$(( total_inventario + ( cantidad * precio )))
+    done < stock.txt
+    
+    echo $total_inventario
